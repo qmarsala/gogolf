d2c9bf612f6444ac04edbb237eeec70293949a1d
Quinten Marsala <quinten.marsala@gmail.com>
2024-04-26 22:02:44 -0700

    take a step back, and maybe diff skills?

--- golfer.go
+++ golfer.go
@@ -1,57 +1,17 @@
 package main
 
-import "fmt"
-
 type Skills struct {
-	Woods   int
-	Irons   int
-	Wedges  int
-	Putting int
+	Recovery int
+	Driving  int
+	Approach int
+	Chipping int
+	Putting  int
 }
 
 type Abilities struct {
-	Power    int
-	Accuracy int
-	Control  int
-}
-
-type ShotShape int
-
-func (ss ShotShape) String() string {
-	switch ss {
-	case -1:
-		return "fade"
-	case 1:
-		return "draw"
-	default:
-		return "straight"
-	}
-}
-
-type Shot struct {
-	Club  Club
-	Shape ShotShape
-	Power int
-}
-
-func (s Shot) String() string {
-	return fmt.Sprintf("club: %s\nshot shape: %s\npower: %d percent",
-		s.Club, s.Shape, s.Power)
-}
-
-type Club struct {
-	Name          string
-	Id            int
-	StockDistance int
-	Skill         string
-}
-
-func (c Club) String() string {
-	return c.Name
-}
-
-type GolfBag struct {
-	Clubs []Club
+	Strength  int
+	Intellect int
+	Control   int
 }
 
 type Golfer struct {
@@ -60,38 +20,12 @@ type Golfer struct {
 	Abilities Abilities
 }
 
-func (g *Golfer) PlayShot(shot Shot) {
-	fmt.Println(shot.String())
-	targetNumber := g.Skills.Wedges + g.Abilities.Accuracy
-	dice := Dice{
-		Sides: 6,
-	}
-	result, rolls := dice.rollN(3)
-	margin := targetNumber - result
-	success := margin >= 0
-
-	fmt.Println(success, targetNumber, result, margin, rolls)
-	if success {
-		// state changes somewhere
-		// we are now shot.distance closer to the hole
-	} else {
-		// how bad did we fail?
-		// critical fail? big margin fail? normal fail?
-		// how did we fail? alignment? chunky? thin?
-		// what is the consequence for the result?
-	}
-}
-
-type Score struct {
-	Hole    Hole
-	Strokes int
-}
-
-type ScoreCard struct {
-	Scores map[int]Score
-}
-
-type GolfBall struct {
-	Lie            string
-	DistanceToHole int
-}
+// func skillCheck(targetNumber int) {
+// 	dice := Dice{
+// 		Sides: 6,
+// 	}
+// 	result, rolls := dice.rollN(3)
+// 	margin := targetNumber - result
+// 	success := margin >= 0
+// 	fmt.Println(success, targetNumber, result, margin, rolls)
+// }
--- main.go
+++ main.go
@@ -9,27 +9,6 @@ import (
 
 func main() {
 	fmt.Println("\nWelcome to GoGolf.")
-	fmt.Println("Please select a skill package:")
-	skill := readString(`
-	1) W 5 - I 3 - W 2 - P 2
-	2) W 3 - I 3 - W 3 - P 3
-	3) W 2 - I 5 - W 2 - P 3
-	4) W 2 - I 2 - W 4 - P 4 
-	`)
-
-	fmt.Println("\nPlease select an ability package:")
-	ability := readString(`
-	1) D 3 - A 2 - C 1
-	2) D 2 - A 2 - C 2
-	3) D 1 - A 3 - C 2
-	4) D 1 - A 1 - C 4 
-	`)
-
-	println(skill)
-	println(ability)
-
-	golfer := Golfer{}
-	playGolf(golfer)
 }
 
 func readString(prompt string) string {
@@ -47,17 +26,4 @@ func playGolf(golfer Golfer) {
 	// execute shot
 	//when hole is complete, move to next hole
 	//when course is complete, end game
-	hole := NewHole(1, 3, 100)
-	fmt.Println(hole)
-
-	golfer.PlayShot(
-		Shot{
-			Club: Club{
-				Name:          "Pitching Wedge",
-				Id:            10,
-				StockDistance: 100,
-			},
-			Shape: 0,
-			Power: 50,
-		})
 }
