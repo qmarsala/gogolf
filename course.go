6eaa9e0a15be90e8382f235e8a0f29a36739bbde
Quinten Marsala <quinten.marsala@gmail.com>
2024-04-27 17:28:28 -0700

    holes need a boundary

--- course.go
+++ course.go
@@ -8,16 +8,18 @@ type Hole struct {
 	Number       int
 	Par          int
 	Distance     Yard
+	Boundary     Size
 	HoleLocation Point
 }
 
-func NewHole(number int, par int, holeLocation Point) *Hole {
-	hole := &Hole{
-		Number:   number,
-		Par:      par,
-		Distance: holeLocation.Distance(Point{0, 0}).Yards(),
+func NewHole(number int, par int, holeLocation Point, boundary Size) *Hole {
+	return &Hole{
+		Number:       number,
+		Par:          par,
+		Distance:     holeLocation.Distance(Point{0, 0}).Yards(),
+		Boundary:     boundary,
+		HoleLocation: holeLocation,
 	}
-	return hole
 }
 
 func (h Hole) String() string {
