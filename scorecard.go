0ba5eaeafa0b130670147a981cecb40dcf195413
Quinten Marsala <quinten.marsala@gmail.com>
2024-04-27 17:58:28 -0700

    add a scorecard

---
+++ scorecard.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"slices"
+)
+
+type ScoreCard struct {
+	Holes  []Hole
+	Scores map[int]int
+}
+
+func (sc ScoreCard) RecordStroke(h Hole) {
+	sc.Scores[h.Number]++
+}
+
+func (sc ScoreCard) TotalStrokes() (score int) {
+	for _, v := range sc.Scores {
+		score += v
+	}
+	return
+}
+
+func (sc ScoreCard) Score() (score int) {
+	for k, v := range sc.Scores {
+		if holeIndex := slices.IndexFunc(sc.Holes, func(h Hole) bool {
+			return h.Number == k
+		}); holeIndex > -1 {
+			score += v - sc.Holes[holeIndex].Par
+		}
+	}
+	return
+}
---
+++ scorecard_test.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestRecordScore(t *testing.T) {
+	hole := Hole{
+		Number: 1,
+		Par:    3,
+	}
+	sc := ScoreCard{
+		Holes:  []Hole{hole},
+		Scores: map[int]int{},
+	}
+
+	sc.RecordStroke(hole)
+	sc.RecordStroke(hole)
+
+	strokes := sc.Scores[hole.Number]
+	if strokes != 2 {
+		t.Error("Expected strokes for hole 1 to be 2, but got", strokes)
+	}
+}
+
+func TestScore(t *testing.T) {
+	hole := Hole{
+		Number: 1,
+		Par:    3,
+	}
+	hole2 := Hole{
+		Number: 2,
+		Par:    4,
+	}
+	sc := ScoreCard{
+		Holes:  []Hole{hole, hole2},
+		Scores: map[int]int{},
+	}
+
+	sc.RecordStroke(hole)
+	sc.RecordStroke(hole)
+
+	sc.RecordStroke(hole2)
+	sc.RecordStroke(hole2)
+	sc.RecordStroke(hole2)
+	sc.RecordStroke(hole2)
+
+	score := sc.Score()
+	if score != -1 {
+		t.Error("Expected score to be -1, but got", score)
+	}
+}
+
+func TestTotalStrokes(t *testing.T) {
+	hole := Hole{
+		Number: 1,
+		Par:    3,
+	}
+	hole2 := Hole{
+		Number: 2,
+		Par:    4,
+	}
+	sc := ScoreCard{
+		Holes:  []Hole{hole, hole2},
+		Scores: map[int]int{},
+	}
+
+	sc.RecordStroke(hole)
+	sc.RecordStroke(hole)
+
+	sc.RecordStroke(hole2)
+	sc.RecordStroke(hole2)
+	sc.RecordStroke(hole2)
+	sc.RecordStroke(hole2)
+
+	strokes := sc.TotalStrokes()
+	if strokes != 6 {
+		t.Error("Expected strokes to be 6, but got", strokes)
+	}
+}
