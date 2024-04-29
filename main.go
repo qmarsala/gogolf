90ea7972b184da3e3be1a78ba7356e6af98ec74c
Quinten Marsala <quinten.marsala@gmail.com>
2024-04-28 21:53:54 -0700

    some easy working golf

--- main.go
+++ main.go
@@ -4,21 +4,29 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
 	fmt.Println("\nWelcome to GoGolf.")
-	ballPosition := Point{X: 1, Y: 2}
-	holePosition := Point{X: 4, Y: 6}
+	hole1 := *NewHole(1, 4, Point{X: 20, Y: int(Yard(423).Units())}, Size{})
+	holes := []Hole{hole1}
+	ball := GolfBall{Location: Point{X: 0, Y: 0}}
+	scoreCard := ScoreCard{
+		Holes:  holes,
+		Scores: map[int]int{},
+	}
 
-	directionToHole := ballPosition.Direction(holePosition)
-
-	fmt.Printf("Direction Vector to Hole: %+v\n", directionToHole)
-	ball := GolfBall{Location: ballPosition}
-	ball.ReceiveHit(Club{Distance: 150}, 1, directionToHole)
-	fmt.Printf("ball pos: %+v\n", ball.Location)
-
+	for ball.Location != hole1.HoleLocation {
+		fmt.Printf("distance to hole: %f\n", ball.Location.Distance(hole1.HoleLocation).Yards())
+		d := readString("Enter distance: ")
+		distance, _ := strconv.ParseFloat(strings.TrimSpace(d), 64)
+		directionToHole := ball.Location.Direction(hole1.HoleLocation)
+		ball.ReceiveHit(Club{Distance: Yard(distance)}, 1, directionToHole)
+		scoreCard.RecordStroke(holes[0])
+		fmt.Printf("%+v (%+v)\n", scoreCard.TotalStrokes(), scoreCard.Score())
+	}
 }
 
 func readString(prompt string) string {
