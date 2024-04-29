af42425277970c8cd60bbf86c0be33696d4309e4
Quinten Marsala <quinten.marsala@gmail.com>
2024-04-28 17:34:41 -0700

    some point to vector conversions

--- world.go
+++ world.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"math"
-)
-
 type Point struct {
 	X int
 	Y int
@@ -14,23 +10,18 @@ type Size struct {
 	Length Unit
 }
 
-type Vector struct {
-	X, Y float64
-}
-
-func (v Vector) Magnitude() float64 {
-	return math.Sqrt(v.X*v.X + v.Y*v.Y)
-}
-
-func (v Vector) Normalize() Vector {
-	mag := v.Magnitude()
-	return Vector{X: v.X / mag, Y: v.Y / mag}
-}
-
-func MovePoint(p Point, direction Vector, distance float64) Point {
-	unit := direction.Normalize()
+func (p Point) Move(direction Vector, distance float64) Point {
+	normalizedDirection := direction.Normalize()
 	return Point{
-		X: int(float64(p.X) + unit.X*distance),
-		Y: int(float64(p.Y) + unit.Y*distance),
+		X: int(float64(p.X) + normalizedDirection.X*distance),
+		Y: int(float64(p.Y) + normalizedDirection.Y*distance),
 	}
+}
+
+func (p Point) Vector() Vector {
+	return Vector{float64(p.X), float64(p.Y)}
+}
+
+func (p1 Point) Direction(p2 Point) Vector {
+	return p2.Vector().Subtract(p1.Vector())
 }
