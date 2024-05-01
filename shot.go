1014a702dfb235272fd1354d76f48a29a599c2b8
Quinten Marsala <quinten.marsala@gmail.com>
2024-04-30 17:38:01 -0700

    brainstorming

---
+++ shot.go
@@ -0,0 +1,10 @@
+package main
+
+//what we need (to start)
+// - a way to indicate a 'fade' or 'draw'
+// - a way for these shots to limit the failure in some way
+//   - instead of it going right or left, if its a draw, it could only go left, by maybe to much left
+// - some sort of result, this should probably include
+// - carry distance
+// - roll distance
+// - list of points to make a path that could change direction (fade or draw) to get around something
